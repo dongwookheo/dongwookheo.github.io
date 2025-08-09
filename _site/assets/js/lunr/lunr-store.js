c194c58a9100f77cbb0f0c71efca876b0f253d2e
dongwookheo <hdwook3918@gmail.com>
2025-08-10 00:59:04 +0900

    fix: 수식 렌더링

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "[T-PAMI 2015] Map-Based Probabilistic Visual Self-Localization",
+        "excerpt":"Author: Marcus A. Brubaker; Andreas Geiger; Raquel Urtasun July 2015 T-PAMI [DOI] [CODE] 서론 자율 시스템의 자기 위치 결정의 중요성 및 기존 기술의 한계 자율 시스템에서 정확하고 효율적인 자기 위치 결정은 핵심적인 과제이다. 이 논문은 두 대의 비디오 카메라와 도로 지도를 유일한 입력으로 사용하여 차량의 자기 위치를 파악하는 저렴한 해결책을...","categories": ["논문리뷰","Computer Vision"],
+        "tags": ["Map-based Visual Localization","Visual Odometry"],
+        "url": "/%EB%85%BC%EB%AC%B8%EB%A6%AC%EB%B7%B0/computer%20vision/pami15-lost/",
+        "teaser": null
+      }]
